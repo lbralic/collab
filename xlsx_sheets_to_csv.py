3113746aad8ae8af4a1e0a67fc1355b2eac6b095
lbralic <114246886+lbralic@users.noreply.github.com>
2023-05-12 16:55:43 -0400

    Add files via upload

---
+++ xlsx_sheets_to_csv.py
@@ -0,0 +1,13 @@
+# Documentation: 
+# Read Excel file - https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
+# Convert to .csv - https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_csv.html
+
+import pandas as pd, os
+
+xlsx_file = r"E:\Documents\Fleming_College\Semester_3\APST62_Collab\Project\Data\BioMonitoringTempData.xlsx"
+output_dir =  r"E:\Documents\Fleming_College\Semester_3\APST62_Collab\Project\Script\Output"
+
+for sheet_name, df in pd.read_excel(xlsx_file, index_col=0, sheet_name=None).items():
+    csv_file = os.path.join(output_dir, f"{sheet_name}.csv")
+    df.to_csv(csv_file, encoding='utf-8')
+
