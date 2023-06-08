1c53004fcecb351a86740e6771f98a476e8ee6f8
lbralic <114246886+lbralic@users.noreply.github.com>
2023-06-08 15:02:50 -0400

    Lucija

--- deliverables/temperature.py
+++ deliverables/temperature.py
@@ -1,40 +1,68 @@
-# TempModel1() >> Temperature Monitoring data processing model 
-# GDBToMap() >> Add all feature classes and tables to the map display
-# AGOLUpload() >> Upload all layers and tables to ArcGIS Online
+# Date last updated: June 8, 2023
 
-# Required edits
-#   Under "Required Edit 1": Enter data/workspace paths
-#   Under "Required Edit 2": Enter the names of the excel files/sheets 
+# Purpose:
+# Processes the temperature data and uploads it to ArcGIS Online (AGOL) as a feature layer.
+# The feature layer will contain a station point layer and a data table.
+#   TempModel() >> Temperature data processing model
+#   GDBToMap() >> Add all feature classes and tables to the map display
+#   GOLUpload() >> Upload all layers and tables to AGOL
 
-# Optional edits
-#   Under "Optional Edit 1": Enter the summary, tags, etc. that will appear on AGOL
-#   Under "Optional Edit 2": Alter the sharing preferences
+# Instructions:
+#   Under "Required inputs", enter the file/folder paths
+#   Under "Optional inputs", enter the name of the feature layer to be uploaded to AGOL,
+#       the summary/tags/etc., and the sharing preferences.
 
-import arcpy, os, pandas as pd
+#############################################
+#####               INPUTS              #####
+#############################################
 
-##########################################
-# >>> Required Edit 1: Input paths
+############## Required inputs ##############
+
+# >>> File/folder paths
 # Path to the folder that contains the TempMonitoring Excel file
-input_Temp_Table = r"C:\Winter2023\COLLAB\Data"
+input_Temp_Table = r"C:\Data"
 # Path to the output TempMonitoring folder - the folder where the csv files are placed
-output_Temp_Table = r"C:\Winter2023\COLLAB\TempDB"
+output_Temp_Table = r"C:\Output"
 # Path to geodatabase 
-ws = r"C:\Winter2023\COLLAB\TempDB\TemperatureMonitoringProject\TemperatureMonitoringProject.gdb"
+ws = r"C:\Project\Project.gdb"
 # Path to .aprx file 
-aprx_path = r"C:\Winter2023\COLLAB\TempDB\TemperatureMonitoringProject\TemperatureMonitoringProject.aprx"
+aprx_path = r"C:\Project\Project.aprx"
 # Empty output folder for the service definition drafts
-outdir = r"C:\Winter2023\COLLAB\TempDB\TemperatureMonitoringProject\Output"
-##########################################
+outdir = r"C:\Output"
 
-
-##########################################
-# >>> Required Edit 2: Enter the names of the excel files/ sheets if they are different  
+# >>> Enter the names of the excel files/ sheets if they are different  
 # Change the keys (left hand side) of this dictionary to match the names of the sheets in the .xlsx file to the names of the tables (right hand side)
 data_names_for_sheet_names = {
     "Coldwater Streams - metadata": "TemperatureMonitoringXYData",
     "ColdwaterStreams": "TemperatureMonitoringData"
 }
-##########################################
+
+
+############## Optional inputs ##############
+
+# >>> Input the name of the feature layer to be uploaded to AGOL
+service_name = "Kawartha Conservation Temperature Data"
+
+# >>> Enter the summary, tags, etc. that will appear on AGOL
+mysummary = "My Summary"
+mytags = "My Tags"
+mydescription = "My Description"
+mycredits = "My Credits"
+myuselimitations = "My Use Limitations"
+
+# >>> Alter sharing preferences
+# Sharing options
+sharepublic = "PRIVATE"                 # Enter "PUBLIC" or "PRIVATE"
+shareorg = "NO_SHARE_ORGANIZATION"      # Enter "SHARE_ORGANIZATION" or "NO_SHARE_ORGANIZATION"
+sharegroup = ""                         # Enter the name of the group(s): "My Group" or ["My Group 1", "My Group 2", ...]
+# AGOL folder name
+foldertype = ""                         # Enter "Existing" to specify an existing folder
+foldername = ""                         # Enter the existing AGOL folder name
+
+########################################################################################
+
+
+import arcpy, os, pandas as pd
 
 # Coordinate system
 coordsys = "PROJCS[\"NAD_1983_CSRS_UTM_Zone_17N\",GEOGCS[\"GCS_North_American_1983_CSRS\",DATUM[\"D_North_American_1983_CSRS\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",500000.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",-81.0],PARAMETER[\"Scale_Factor\",0.9996],PARAMETER[\"Latitude_Of_Origin\",0.0],UNIT[\"Meter\",1.0]]"
@@ -62,8 +90,6 @@ aprx.save()
 
 # Excel File Data Pre-Processing - makes .xslx file into .csvs and removes any spaces from field names.
 def xlsx_sheets_to_csv(path_to_excel_file, output_folder):
-    print(">> Processing the Excel table...") 
-
     for sheet_name, df in pd.read_excel(path_to_excel_file, index_col=0, sheet_name=None).items():
         csv_file = os.path.join(output_folder, f"{sheet_name}.csv")
         # Remove spaces in field names
@@ -75,6 +101,7 @@ def xlsx_sheets_to_csv(path_to_excel_file, output_folder):
 def GDBToMap(fcs, tables):
     print(">> Adding data to map...")
     # Add stations (point layer)
+    print("       Adding stations")
     for fc in fcs:
         arcpy.management.MakeFeatureLayer(fc, fc)
         lyr_name = "{}.lyrx".format(fc)
@@ -83,6 +110,7 @@ def GDBToMap(fcs, tables):
         lyr = arcpy.mp.LayerFile(lyr_path)
         m.addLayer(lyr)
     # Add data (table)
+    print("       Adding table")
     for table in tables:
         table_path = os.path.join(ws, table)
         addTab = arcpy.mp.Table(table_path)
@@ -120,19 +148,16 @@ def AGOLUpload(service_name):  # service_name is the name of the feature layer t
     for x in range(count_tables):
         lyr_list.append(tables[x])
 
-    ##########################################
     # Create FeatureSharingDraft and enable overwriting
-    # >>> Optional Edit 1: Enter the summary, tags, etc. that will appear on AGOL
     server_type = "HOSTING_SERVER"
     # Parameters: getWebLayerSharingDraft(server_type, service_type, service_name, {layers_and_tables})
     sddraft = m.getWebLayerSharingDraft(server_type, "FEATURE", service_name, lyr_list)
-    sddraft.summary = "My Summary"
-    sddraft.tags = "My Tags"
-    sddraft.description = "My Description"
-    sddraft.credits = "My Credits"
-    sddraft.useLimitations = "My Use Limitations"
+    sddraft.summary = mysummary
+    sddraft.tags = mytags
+    sddraft.description = mydescription
+    sddraft.credits = mycredits
+    sddraft.useLimitations = myuselimitations
     sddraft.overwriteExistingService = True
-    ##########################################
 
     # Create Service Definition Draft file
     # Parameters: exportToSDDraft(out_sddraft)
@@ -143,19 +168,16 @@ def AGOLUpload(service_name):  # service_name is the name of the feature layer t
     # Parameters: arcpy.server.StageService(in_service_definition_draft, out_service_definition, {staging_version})
     arcpy.server.StageService(sddraft_output_filename, sd_output_filename)
 
-    ##########################################
     # Share to portal
-    # >>> Optional Edit 2: Alter sharing preferences
     # Documentation: https://pro.arcgis.com/en/pro-app/latest/tool-reference/server/upload-service-definition.htm
     inOverride = "OVERRIDE_DEFINITION"
     # Sharing options
-    inSharePublic = "PRIVATE"                 # Enter "PUBLIC" or "PRIVATE"
-    inShareOrg = "NO_SHARE_ORGANIZATION"      # Enter "SHARE_ORGANIZATION" or "NO_SHARE_ORGANIZATION"
-    inShareGroup = ""                         # Enter the name of the group(s): "My Group" or ["My Group 1", "My Group 2", ...]
+    inSharePublic = sharepublic
+    inShareOrg = shareorg
+    inShareGroup = sharegroup
     # AGOL folder name
-    inFolderType = ""                         # Enter "Existing" to specify an existing folder
-    inFolderName = ""                         # Enter the existing AGOL folder name
-    ##########################################
+    inFolderType = foldertype
+    inFolderName = foldername
 
     print(">> Start Uploading")
     # Parameters: arcpy.server.UploadServiceDefinition(in_sd_file, in_server, {in_service_name}, {in_cluster}, {in_folder_type}, {in_folder}, {in_startupType}, {in_override}, {in_my_contents}, {in_public}, {in_organization}, {in_groups})
@@ -185,6 +207,7 @@ def TempModel(data_names_for_sheet_names):
     
     ##### Extract .csv files from all the .xlsx files #####
     files_in_xlsx_folder = os.listdir(input_Temp_Table)
+    print("       Processing the Excel table") 
     for filename in files_in_xlsx_folder:
         # skip anything in the folder that isn't a .xlsx
         if filename.endswith(".xlsx") == False:
@@ -265,5 +288,4 @@ if __name__ == '__main__':
     with arcpy.EnvManager(outputCoordinateSystem = coordsys, scratchWorkspace = ws, workspace = ws):
         TempModel(data_names_for_sheet_names)
         GDBToMap(["TemperatureMonitoringPoints"], ["TemperatureMonitoringData"])
-        # AGOLUpload("Kawartha Conservation Temperature Monitoring Data")
-        AGOLUpload("Temperature Monitoring")
+        AGOLUpload(service_name)
